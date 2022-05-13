7a8df486007de3dfd62c5b8f953add387486cc1e
vikkumar2021 <103162272+vikkumar2021@users.noreply.github.com>
2022-05-13 18:01:26 -0400

    v23 changes

---
+++ components/Map.js
@@ -0,0 +1,44 @@
+import { useEffect, useContext } from 'react'
+import mapboxgl from 'mapbox-gl'
+import { UberContext } from '../context/uberContext'
+
+const style = {
+  wrapper: `flex-1 h-full w-full`,
+}
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoidmlra3VtYXJyZWNlaXB0cyIsImEiOiJjbDM0ejI0OG4xOGNvM2pyeGVobm9xaGdxIn0.yM7DpRqNPAWgD6Hpe-x6eg'
+
+const Map = () => {
+  const { pickupCoordinates, dropoffCoordinates } = useContext(UberContext)
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+      center: [-99.29011, 39.39172],
+      zoom: 3,
+    })
+
+    if (pickupCoordinates) {
+      addToMap(map, pickupCoordinates)
+    }
+
+    if (dropoffCoordinates) {
+      addToMap(map, dropoffCoordinates)
+    }
+
+    if (pickupCoordinates && dropoffCoordinates) {
+      map.fitBounds([dropoffCoordinates, pickupCoordinates], {
+        padding: 400,
+      })
+    }
+  }, [pickupCoordinates, dropoffCoordinates])
+
+  const addToMap = (map, coordinates) => {
+    const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map)
+  }
+
+  return <div className={style.wrapper} id='map' />
+}
+
+export default Map
